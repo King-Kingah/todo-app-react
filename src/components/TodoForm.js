51c2529f7a8d361e53712b47749f196f60c5e22f
King-Kingah <kingajames2015@gmail.com>
2023-04-10 15:55:43 +0300

    Add a todos functionality using props.

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,11 +1,19 @@
-import React, {useState} from 'react'
+import React, {useState} from 'react';
 
-export const TodoForm = () => {
+export const TodoForm = ({addTodo}) => {
     const [value, setValue] = useState('');
 
+    const handleSubmit = e => {
+      e.preventDefault();
+
+      addTodo(value)
+
+      setValue("")
+    }
+
   return (
-    <form className='TodoForm'>
-        <input type='text' className='todo-list' placeholder='What task will you handle today?' onChange={(e) => console.log(e.target.value)}/>
+    <form className='TodoForm' onSubmit={handleSubmit}>
+        <input type='text' className='todo-input' value={value} placeholder='What task will you handle today?' onChange={(e) => setValue(e.target.value)}/>
         <button type='submit' className='todo-btn'>Add Task</button>
     </form>
   )

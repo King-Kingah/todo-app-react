7049150605de886ca7817f97146571352d650fc9
King-Kingah <kingajames2015@gmail.com>
2023-04-10 15:25:16 +0300

    Correct onChange state typo and install packages.

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -5,7 +5,7 @@ export const TodoForm = () => {
 
   return (
     <form className='TodoForm'>
-        <input type='text' className='todo-list' placeholder='What task will you handle today?' onChangnge={(e) => console.log(e.target.value)}/>
+        <input type='text' className='todo-list' placeholder='What task will you handle today?' onChange={(e) => console.log(e.target.value)}/>
         <button type='submit' className='todo-btn'>Add Task</button>
     </form>
   )
